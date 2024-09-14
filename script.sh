28331bdaac4478ab210a4c722eb4d2b37d6c0aab
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-09-14 12:54:58 +0900

    不要なenvファイル読み込みと、不要な関数を削除

--- script.sh
+++ script.sh
@@ -3,18 +3,13 @@
 # ファイル名: script.sh
 # 
 # 概要: crontabで登録した実行日時(平日のAM8:40に実行)に mtgNotif.js を実行する為のスクリプト
-# crontabの内容: 40 8 * * 1-5 /Users/shiratorinaoki/projects/NotifMTG/src/script.sh
+# crontabの内容: 
+# 45 8 * * * /bin/zsh -c 'source /Users/shiratorinaoki/projects/NotifMTG/.env && /bin/zsh /Users/shiratorinaoki/projects/NotifMTG/script.sh' >> /Users/shiratorinaoki/projects/NotifMTG/logs/cron.log 2>&1
 #
 # 作成者: 00083ns
 # 作成日: 2024/08/25
 # 更新者: 
 # 更新日: 
-#
-
-# .envファイルを読み込む
-set -a
-source ~/projects/NotifMTG/.env
-set +a
 
 # Slackが起動していない場合、Slackを起動
 if ! pgrep -x "Slack" > /dev/null; then
@@ -22,9 +17,6 @@ if ! pgrep -x "Slack" > /dev/null; then
   open -a "Slack"  # Slackを起動
   sleep 5 # 完全に起動するまで少し待つ
 fi
-
-# Slackの起動を待機
-wait_for_slack
 
 # プロセスを停止（前日のプロセスを終了）
 pid=$(ps aux | grep 'src/mtgNotif.js' | grep -v grep | awk '{print $2}')
